4fc552db9e8b2a9610b0386b33a0d919a2f5171f
Chris Done <chrisdone@gmail.com>
2013-03-10 18:55:47 +0100

    Fix chart scaling.

--- src/Ircbrowse/View/Chart.hs
+++ src/Ircbrowse/View/Chart.hs
@@ -8,18 +8,18 @@ module Ircbrowse.View.Chart where
 
 import Ircbrowse.View
 
-barChart :: Integral a => [(String, a)] -> Html
+barChart :: (Show a,Integral a) => [(String, a)] -> Html
 barChart values = p $ img ! src (toValue url)
 
   where url = "http://chart.apis.google.com/chart?" ++
-              "chyl=0:|" ++ intercalate "|" ylabels ++
-              "&chxl=0:|" ++ intercalate "|" xlabels ++
+              "chxl=0:|" ++ intercalate "|" xlabels ++
               "&chxt=x,y&chd=t:" ++
               intercalate "," datas ++
-              "&chs=" ++ show w ++ "x" ++ show h ++ "&cht=bvs&chbh=a"
+              "&chs=" ++ show w ++ "x" ++ show h ++ "&cht=bvs&chbh=a" ++
+              "&chxr=0|1,0," ++ show maxcount
         xlabels = map fst values
         ylabels = map show [10,20,30,40,50]
-        datas = map (\x -> show (round ((fi x / maxcount) * 61))) times
+        datas = map (\x -> show (round ((fi x / maxcount) * 100))) times
         times = map snd values
         maxcount = fi (maximum times)
         w = 450
